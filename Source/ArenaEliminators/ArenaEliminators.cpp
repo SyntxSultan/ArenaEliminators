4e34de57cde6c2e3d7269fa84cf44b288a672b30
Taha Eren Afacan <sessizyayinci@gmail.com>
2024-05-16 02:31:48 +0300

    Initial Project Files

---
+++ Source/ArenaEliminators/ArenaEliminators.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaEliminators.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaEliminators, "ArenaEliminators" );
